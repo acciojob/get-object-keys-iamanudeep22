10f489205d820dd702b33af310be23e41ca27988
Anudeep KN <116871481+iamanudeep22@users.noreply.github.com>
2025-01-26 08:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let student = {
+	name : "Anudeep"
+}
+
+let arr = [];
+
+function getKeys(obj){
+	for(key in obj){
+        arr.push(key);
+    }
+    return arr;
+}
+
+console.log(getKeys(student));
